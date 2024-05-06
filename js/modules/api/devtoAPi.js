5b4ee53287110f5f7dc721e3856908cdcf2cb63a
Gloriela Suarez <glo.suacas@gmail.com>
2024-05-06 12:18:04 -0600

    api y details, sin activar

---
+++ js/modules/api/devtoAPi.js
@@ -0,0 +1,26 @@
+const BASE_URL = "https://javascriptglo-default-rtdb.firebaseio.com/posts";
+
+const createPosts = async (postObject) => {
+  let response = await fetch(`${BASE_URL}/.json`, {
+    method: "POST",
+    body: JSON.stringify(postObject),
+  });
+  let data = await response.json();
+  return data;
+};
+
+const fetchPostByKey = async (postKey) => {
+  let response = await fetch(`${BASE_URL}/${postKey}/.json`);
+  let data = await response.json();
+  return data;
+};
+
+const fetchAllPosts = async () => {
+  let response = await fetch(`${BASE_URL}/.json`);
+  let data = await response.json();
+  let keys = Object.keys(data);
+  let postsArray = keys.map((key) => ({ ...data[key], key }));
+  return postsArray;
+};
+
+export { createPosts, fetchPostByKey, fetchAllPosts };
