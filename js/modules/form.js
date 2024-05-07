dbcb33031d24807b35bf77935b767de72eb291a4
MaryChuyM <moremard@gmail.com>
2024-05-06 20:27:27 -0600

    deleted form.js

--- js/modules/form.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('login-button').addEventListener('click', function() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    console.log('Correo electrónico:', email);
-    console.log('Contraseña:', password);
-});
