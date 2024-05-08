ab55a52fcfc5056871e34d39e3428a8311b8fc86
Jorge Escobedo <jorge.luis.es612@outlook.com>
2024-05-08 11:18:39 -0600

    editando archivo devtoApi.js

--- js/modules/api/devtoAPi.js
+++ js/modules/api/devtoAPi.js
@@ -1,7 +1,7 @@
-const BASE_URL = "https://reto-js-10b99-default-rtdb.firebaseio.com/blogDevTo/.json";
+const BASE_URL = "https://reto-js-10b99-default-rtdb.firebaseio.com/blogDevTo";
 //Funcion para traer los posts de la DB
 const traerPost = async () => {
-    let response = await fetch(BASE_URL);
+    let response = await fetch(`${BASE_URL}/.json`);
     let posts = await response.json();
     let keys = Object.keys(posts);
     let arregloPosts = keys.map((key) => {
