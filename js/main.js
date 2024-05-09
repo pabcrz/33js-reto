e4c5b0981f187939f01a760698e42d9edf2a2d93
Gloriela Suarez <glo.suacas@gmail.com>
2024-05-08 22:12:09 -0600

    arreglos en el estilo

--- js/main.js
+++ js/main.js
@@ -94,13 +94,15 @@ const creacionPost = (post,index) => {
     contenedorEmojisComentarios.append(contenedorEmojis);
 
     let toDetails = document.createElement("a");
+    toDetails.classList.add("linkToPost")
     toDetails.setAttribute("href", `../views/detailsPost.html?postKey=${key}`);
+    toDetails.style.textDecoration = 'none'
     console.log('key,',key)
     toDetails.append(contenedorTexto)
 
     index === 0 ? 
     contenedorPost.append(contenedorImagen, contenedorUsuario, toDetails, contenedorEmojisComentarios) :
-    contenedorPost.append(contenedorUsuario, contenedorTexto, contenedorEmojisComentarios);
+    contenedorPost.append(contenedorUsuario, toDetails, contenedorEmojisComentarios);
 
     return contenedorPost;
 };
