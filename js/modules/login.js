391fea9f95359195c879ea7eb92641378c373885
MaryChuyM <moremard@gmail.com>
2024-05-09 18:36:38 -0600

    Added login view

--- js/modules/login.js
+++ js/modules/login.js
@@ -15,9 +15,14 @@ logInBtn.addEventListener("click",( ) => {
      /*Token simulado*/
      let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
     
+
+    
      localStorage.setItem("token", token);
      window.open("../post.html", "_blank");
 });
+
+
+
 
 
 
